d5c94c105e70ebfed2a1b65efdde5dad2c0e1c20
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-25 16:31:03 +0000

    feat: add db_start and db_end

--- pages/api/v1/create.js
+++ pages/api/v1/create.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=3');
 
+  const db_start = performance.now();
   const client = await getDB().connect();
-
   const { date, runtime } = JSON.parse(req.body);
 
   try {
@@ -13,11 +13,14 @@ export default async function handler(req, res) {
       date,
       runtime,
     ]);
+    const db_end = performance.now();
 
     res.status(200).json({
       message: 'Create v1 - A-OK!',
       results: {
         id: response.rows[0].id,
+        db_start: db_start,
+        db_end: db_end,
       },
     });
   } catch (error) {
