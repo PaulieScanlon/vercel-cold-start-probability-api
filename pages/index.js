5e20232b772ea9073e7d849bcdc2aa60deb802ab
Paul Scanlon <pauliescanlon@gmail.com>
2023-03-25 08:18:10 +0000

    test: add client side diff

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const Page = () => {
   const handleCreate = async () => {
+    const start = performance.now();
+    console.log('start: ', start);
     try {
       const response = await fetch('./api/create', {
         method: 'POST',
@@ -14,7 +16,10 @@ const Page = () => {
         throw new Error('Bad response');
       }
 
+      const end = performance.now();
+      console.log('end: ', end);
       const json = await response.json();
+      console.log('diff: ', ((end - start) / 1000).toFixed(2));
       console.log(JSON.stringify(json, null, 2));
     } catch (error) {
       console.error();
